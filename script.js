c7df4a52cc9472737fe397c7c188177458c974bc
AdwikaVishal <168633141+AdwikaVishal@users.noreply.github.com>
2025-04-10 12:57:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById('video');
-
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-        video.srcObject = stream;
-    })
-    .catch(error => {
-        console.error("Error accessing the camera: ", error);
-    });
